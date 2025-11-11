8b8d49efe0933e40df02d1cb92fcb5d2fed418c6
Yeni Kurnia Fitri <yenikurniafitri4@gmail.com>
2025-11-11 13:11:10 +0700

    Add files via upload

---
+++ CLTugas2_yeni.py
@@ -0,0 +1,27 @@
+#python 3.7.1
+
+class Alatmusik :
+    nama1 ="seluing"
+    cara_memainkan1 ="ditiup"
+    Asal_daerah1 ="jawa barat "
+    Bentuk1 ="panjang"
+    Terbuatdari1 ="bambu" 
+    nama2 ="Gong" 
+    cara_memainkan2 ="dipukul"
+    Asal_daerah2 ="jawa timur"
+    Bentuk2 ="bulat"
+    Terbuatdari2 ="campuran perunggu"
+    
+alatmusik1 = Alatmusik()
+alatmusik2 = Alatmusik()
+
+print(alatmusik1.nama1)
+print(alatmusik1.cara_memainkan1)
+print(alatmusik1.Asal_daerah1)
+print(alatmusik1.Bentuk1)
+print(alatmusik1.Terbuatdari1)
+print(alatmusik2.nama2)
+print(alatmusik2.cara_memainkan2)
+print(alatmusik2.Asal_daerah2)
+print(alatmusik2.Bentuk2)
+print(alatmusik2.Terbuatdari2)
